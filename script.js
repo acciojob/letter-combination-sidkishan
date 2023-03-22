d4bb40a19553c4732945a96ee33ca70acd7bb26a
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-03-22 12:37:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var obj11 = {
+var keys = {
   2: "abc",
   3: "def",
   4: "ghi",
@@ -9,24 +9,25 @@ var obj11 = {
   9: "wxyz",
 };
 function letterCombinations(input_digit) {
-  if (input_digit.length == 0) {
-    var arr = [];
-    arr.push("");
-    return arr;
-  }
-  let fd = input_digit[0];
-  let ans = [];
-  let ros = input_digit.substring(1);
-  let str = obj11[fd];
-  for (let i = 0; i < str.length; i++) {
-    let fc = str[i];
-    let smallans = letterCombinations(ros);
-    for (var val of smallans) {
-      let smans = fc + val;
-      ans.push(smans);
-    }
-  }
-  return ans;
+  //base case
+	if(input_digit.length == 0){
+		var base = [];
+		base.push("");
+		return base;
+	}
+	var fd = input_digit.charAt(0);
+	var rest = input_digit.substring(1);
+	var small = letterCombinations(rest);
+	var ans = [];
+	for(var i = 0; i < keys[fd].length; i++){
+		for(var j = 0; j < small.length; j++){
+			var str = keys[fd].charAt(i) + small[j];
+			ans.push(str);
+		}
+	}
+	return ans;
 }
 
 module.exports = letterCombinations;
+
+
